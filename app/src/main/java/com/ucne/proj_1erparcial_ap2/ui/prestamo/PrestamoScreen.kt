55e55bc1041bb0e0898c447c603e341ebbea2499
elianny199920@gmail.com <104779804+elianny-2022@users.noreply.github.com>
2023-03-09 18:25:10 -0400

    agregando validacion

--- app/src/main/java/com/ucne/proj_1erparcial_ap2/ui/prestamo/PrestamoScreen.kt
+++ app/src/main/java/com/ucne/proj_1erparcial_ap2/ui/prestamo/PrestamoScreen.kt
@@ -9,21 +9,23 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Save
+import androidx.compose.material.icons.twotone.Error
 import androidx.compose.material3.*
 import androidx.compose.material3.R
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ucne.proj_1erparcial_ap2.data.local.entity.PrestamoEntity
@@ -61,7 +63,7 @@ private fun PrestamoBody(
                 .padding(8.dp)
                 .fillMaxWidth(),
             value = viewModel.deudor,
-            onValueChange = { viewModel.deudor = it },
+            onValueChange = viewModel::onDeudorChanged,
             leadingIcon = {
                 Icon(
                     imageVector = Icons.Filled.Person,
@@ -77,14 +79,28 @@ private fun PrestamoBody(
                     fontStyle = FontStyle.Italic,
                     fontWeight = FontWeight.Bold
                 )
+            },
+            isError = viewModel.deudorError.isNotBlank(),
+            trailingIcon = {
+                if(viewModel.deudorError.isNotBlank()){
+                    androidx.compose.material3.
+                    Icon(imageVector = Icons.TwoTone.Error, contentDescription = "error")
+                }
             }
         )
+        if (viewModel.deudorError.isNotBlank()) {
+            Text(
+                text = viewModel.deudorError,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+
         OutlinedTextField(
             modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth(),
             value = viewModel.concepto,
-            onValueChange = { viewModel.concepto = it },
+            onValueChange =viewModel::onConceptoChanged,
             leadingIcon = {
                 Icon(
                     imageVector = Icons.Filled.TextSnippet,
@@ -101,14 +117,27 @@ private fun PrestamoBody(
                     fontWeight = FontWeight.Bold
                 )
             },
-
+            isError = viewModel.conceptoError.isNotBlank(),
+            trailingIcon = {
+                if(viewModel.conceptoError.isNotBlank()){
+                    androidx.compose.material3.
+                    Icon(imageVector = Icons.TwoTone.Error, contentDescription = "error")
+                }
+            }
+        )
+        if (viewModel.conceptoError.isNotBlank()) {
+            Text(
+                text = viewModel.conceptoError,
+                color = MaterialTheme.colorScheme.error
             )
+        }
+
         OutlinedTextField(
             modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth(),
             value = viewModel.monto,
-            onValueChange = { viewModel.monto = it },
+            onValueChange = viewModel::onMontoChanged,
             leadingIcon = {
                 Icon(
                     imageVector = Icons.Filled.AttachMoney,
@@ -124,13 +153,29 @@ private fun PrestamoBody(
                     fontStyle = FontStyle.Italic,
                     fontWeight = FontWeight.Bold
                 )
-            }
+            },
+            isError = viewModel.montoError.isNotBlank(),
+            trailingIcon = {
+                if (viewModel.montoError.isNotBlank()) {
+                    Icon(imageVector = Icons.TwoTone.Error, contentDescription = "error")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal
+            )
         )
+        if (viewModel.montoError.isNotBlank()) {
+            Text(
+                text = viewModel.montoError,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+
         Spacer(modifier = Modifier.height(30.dp))
         ExtendedFloatingActionButton(
             modifier = Modifier
                 .size(100.dp, 80.dp)
-                .align( Alignment.End)
+                .align(Alignment.End)
                 .wrapContentSize(Alignment.Center),
             shape = CircleShape,
             text = {
@@ -213,3 +258,95 @@ private fun PrestamoRow(prestamo: PrestamoEntity) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showSystemUi = true)
+@Composable
+fun MyPreview() {
+    var deudor by remember {
+        mutableStateOf("")
+    }
+    val deudorError by remember {
+        mutableStateOf("")
+    }
+    var concepto by remember {
+        mutableStateOf("")
+    }
+    val conceptoError by remember {
+        mutableStateOf("")
+    }
+    var monto by remember {
+        mutableStateOf("")
+    }
+    val montoError by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = deudor,
+            onValueChange = { deudor = it },
+            label = { Text("Deudor") },
+            isError = deudorError.isNotBlank(),
+            trailingIcon = {
+                if (deudorError.isNotBlank()) {
+                    Icon(imageVector = Icons.TwoTone.Error, contentDescription = "error")
+                }
+            }
+        )
+        if (deudorError.isNotBlank()) {
+            Text(
+                text = deudorError,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = concepto,
+            onValueChange = { concepto = it },
+            label = { Text("Concepto") },
+            isError = conceptoError.isNotBlank(),
+            trailingIcon = {
+                if (conceptoError.isNotBlank()) {
+                    Icon(imageVector = Icons.TwoTone.Error, contentDescription = "error")
+                }
+            }
+        )
+        if (conceptoError.isNotBlank()) {
+            Text(
+                text = conceptoError,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = monto,
+            onValueChange = { monto = it },
+            label = { Text("Monto") },
+            isError = deudorError.isNotBlank(),
+            trailingIcon = {
+                if (deudorError.isNotBlank()) {
+                    Icon(imageVector = Icons.TwoTone.Error, contentDescription = "error")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal
+            )
+        )
+        if (deudorError.isNotBlank()) {
+            Text(
+                text = montoError,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+
+    }
+}
--- app/src/main/java/com/ucne/proj_1erparcial_ap2/ui/prestamo/PrestamoViewModel.kt
+++ app/src/main/java/com/ucne/proj_1erparcial_ap2/ui/prestamo/PrestamoViewModel.kt
@@ -22,8 +22,11 @@ class PrestamoViewModel @Inject constructor(
     private val prestamoRepository: PrestamoRepository
 ) : ViewModel() {
     var deudor by mutableStateOf("")
+    var deudorError by mutableStateOf("")
     var concepto by mutableStateOf("")
+    var conceptoError by mutableStateOf("")
     var monto by mutableStateOf("")
+    var montoError by mutableStateOf("")
 
     var uiState = MutableStateFlow(PrestamosUiState())
         private set
@@ -39,17 +42,58 @@ class PrestamoViewModel @Inject constructor(
             }
         }
     }
+    fun onDeudorChanged(deudor: String) {
+        this.deudor = deudor
+        HayErrores()
+    }
+    fun onConceptoChanged(concepto: String){
+        this.concepto = concepto
+        HayErrores()
+    }
+
+    fun onMontoChanged(monto: String) {
+        this.monto = monto
+        HayErrores()
+    }
+
     fun insertar() {
+
+        if (HayErrores())
+            return
+
         val prestamo = PrestamoEntity(
             deudor = deudor,
             concepto = concepto,
             monto = monto.toDoubleOrNull() ?: 0.0
         )
+
         viewModelScope.launch(Dispatchers.IO) {
             prestamoRepository.insert(prestamo)
             Limpiar()
         }
     }
+
+    private fun HayErrores(): Boolean {
+        var hayError = false
+        deudorError = ""
+        if (deudor.isBlank()) {
+            deudorError = "Debe indicar el deudor"
+            hayError = true
+        }
+        conceptoError = ""
+        if(concepto.isBlank()){
+            conceptoError = "Debe indicar el concepto"
+            hayError = true
+        }
+
+        montoError = ""
+        if ((monto.toDoubleOrNull() ?: 0.0) <= 0.0) {
+            montoError = "Debe indicar un monto mayor que cero"
+            hayError = true
+        }
+        return hayError
+    }
+
     private fun Limpiar() {
         deudor = ""
         concepto = ""
